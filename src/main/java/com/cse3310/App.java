8447f584750aea612292a9b5ed9a1ad588aabf07
LToothbrush <137121223+LToothbrush@users.noreply.github.com>
2024-03-17 02:55:55 -0500

    Added Maven build and java classes

---
+++ src/main/java/com/cse3310/App.java
@@ -0,0 +1,63 @@
+package com.cse3310;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+/**
+ * Hello world!
+ *
+ */
+public class App extends WebSocketServer
+{
+   
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onClose'");
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onMessage'");
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onError'");
+    }
+
+    @Override
+    public void onStart() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onStart'");
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onOpen'");
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
