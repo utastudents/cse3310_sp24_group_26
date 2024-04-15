9dfb1421a7d07f8bd00306be4662cc054e4b356a
LToothbrush <137121223+LToothbrush@users.noreply.github.com>
2024-04-15 23:47:22 +0000

    Added words into grid with bugs

--- src/main/java/com/cse3310/Game.java
+++ src/main/java/com/cse3310/Game.java
@@ -29,22 +29,26 @@ public class Game{
     }
 
     public char[][] generateGrid(ArrayList<String> words,ArrayList<String> wordbank){
-        char[][] grid = new char[50][50];
+
         Random rand = new Random();
         int length = 25;
         int correctWords = 15;
         int width = 25;
+        char[][] grid = new char[50][50];
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String insertWord;
+        String insertWord; 
         int orientation; //Integer to determine what direction the word will be printed
         int startY;
         int startX;
+        int dX;
+        int dY;
+        boolean fits;
        //Input words into the array
         wordBank = new ArrayList<String>();
 
          for(int a = 0; a < correctWords; a ++)
         {
-            wordBank.add(words.get(rand.nextInt(words.size())));
+            wordBank.add(words.get(rand.nextInt(words.size())).toUpperCase());
             System.out.println(wordBank.get(a));
         }
 
@@ -53,23 +57,35 @@ public class Game{
             insertWord = wordBank.get(b);
             do
             {
+                fits = true;
                 startX = rand.nextInt(length - 1);
                 startY = rand.nextInt(width - 1);
-                orientation = rand.nextInt(4);
-            }while((((length - 1) - startX) - wordBank.get(b).length() < 0) && (((width - 1) - startY) - wordBank.get(b).length() < 0));
-
-            switch(orientation)
-            {
-                default: //horizontal
-                for(int c = 0; c < wordBank.get(b).length(); c++)
+                orientation = rand.nextInt(8);
+                dX = orientation / 3 - 1;
+                dY = orientation % 3 - 1;
+                for(int d = 0; d < insertWord.length(); d++ )
                 {
-                    grid[startY][startX+c] = insertWord.charAt(c);
+                    if(startX + (d * dX) > length - 1 || startX + (d * dX) < 0 || startY + (d * dY) > width - 1 || startY + (d * dY) < 0 || grid[startY + (d * dY)][startX + (d * dX)] != 0)
+                    {
+                        fits = false;
+                        
+                    }
                 }
-                break;
+            }while(fits == false);
 
-            }
+
+            for(int c = 0; c < insertWord.length() ; c++)
+                {
+                    grid[startY + (c * dY)][startX + (c * dX)] = insertWord.charAt(c);
+                }
+
+                
+            
+
+            
             
         }
+
         for(int i = 0;i < width; i++)
         {
             for(int j = 0; j < length; j++)
