8aea4c788244e8957aee81f7d01fb22817403a67
Kristal6113 <kxp6113@mavs.uta.edu>
2024-04-24 12:29:13 -0500

    add game variables to user.java

--- src/main/java/com/cse3310/User.java
+++ src/main/java/com/cse3310/User.java
@@ -6,12 +6,16 @@ public class User
     public String username;
     public int wordCount;
     public int gameWon;
+    public int gamesLost;
+    public int totalGamesPlayed;
 
     public User(String username)
     {
         this.username = username;
         this.wordCount = 0;
         this.gameWon = 0;
+        this.totalGamesPlayed = 0;
+        this.gamesLost = totalGamesPlayed-gameWon;
     }
 
     public void setName(String newUsername){
