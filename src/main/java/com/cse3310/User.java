a1fd42c3c577b6bc9999d43a2f15fca232c82564
Whiton Nguyen <whitonnguyen@Whitons-MBP.attlocal.net>
2024-04-24 22:00:19 -0500

    Done with Lobby and OnClose

--- src/main/java/com/cse3310/User.java
+++ src/main/java/com/cse3310/User.java
@@ -1,15 +1,18 @@
 package com.cse3310;
 
+import org.java_websocket.WebSocket;
 
 public class User
 {
     public String username;
     public int wordCount;
     public int gameWon;
+    public WebSocket conn;
 
-    public User(String username)
+    public User(String username, WebSocket conn)
     {
         this.username = username;
+        this.conn = conn;
         this.wordCount = 0;
         this.gameWon = 0;
     }
