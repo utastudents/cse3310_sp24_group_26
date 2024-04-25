30a0cadc8af6060c27499af73f203b310f8cab76
Kristal6113 <kxp6113@mavs.uta.edu>
2024-04-24 19:47:52 -0500

    get rid of unneeded space

--- src/test/java/com/cse3310/GameTest.java
+++ src/test/java/com/cse3310/GameTest.java
@@ -6,19 +6,18 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class GameTest {
-    Game game = new Game();
+    //Game game = new Game();
     User user = new User("user");
     ArrayList<User> users = new ArrayList<>();
     /**
-     * if a player's score equals 4 their wins should equal 4
+     * if a player's score equals 4
      */
     @Test
     public void addFourToScoreAndWins()
     {
         users.add(user);
         user.gameWon+=4;
-        game.checkWin(user);
-        //assertTrue(users.contains(user));
+        //game.checkWin(user);
         assertEquals(4, user.gameWon);
         
     }
