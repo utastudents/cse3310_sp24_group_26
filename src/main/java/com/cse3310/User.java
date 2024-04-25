d9b0d0d96dec5a33a6c69b244b3de75a9b33383a
Kristal6113 <157341679+Kristal6113@users.noreply.github.com>
2024-04-25 12:33:12 -0500

    Revert "Merge branch 'kristal' into main"
    This reverts commit b5f93fd4e89986bdeb743e1157483bd458d4d5c1, reversing
    changes made to 18a81edfd456e3b5388230b676ede0de630c54f0.

--- src/main/java/com/cse3310/User.java
+++ src/main/java/com/cse3310/User.java
@@ -7,12 +7,8 @@ public class User
     public String username;
     public int wordCount;
     public int gameWon;
-    public int gamesLost;
-    public int totalGamesPlayed;
-
     public String color;
     public WebSocket conn;
-
 
     public User(String username, WebSocket conn)
     {
@@ -20,8 +16,6 @@ public class User
         this.conn = conn;
         this.wordCount = 0;
         this.gameWon = 0;
-        this.totalGamesPlayed = 0;
-        this.gamesLost = totalGamesPlayed-gameWon;
     }
 
     public void setName(String newUsername){
