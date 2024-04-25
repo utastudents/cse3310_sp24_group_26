08490e782d6658e13bf480a5ef391fe144a0daa4
NotVeridion <mdn8457@mavs.uta.edu>
2024-04-25 18:30:30 -0500

    Implemented ready system. Lobby will wait for enough ready players

---
+++ src/main/java/com/cse3310/Error.java
@@ -0,0 +1,11 @@
+package com.cse3310;
+
+public class Error {
+    String user;
+    String error;
+
+    public Error(String user, String error) {
+        this.user = user;
+        this.error = error;
+    }
+}
